60905c4d53617c3c47055034673c4ff1e96b43cc
Shahriar <sh.minaee@gmail.com>
2026-02-11 21:20:44 -0700

    updated settings.py

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tp@t!u^h-95#!#!(t!-s5&u+70=isidk2!s6^3o9f552w3vk76
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['minaee.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
