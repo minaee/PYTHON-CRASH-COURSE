caa3e9ed90262105dcebb63622d1b276ef6a8c0b
Shahriar <sh.minaee@gmail.com>
2026-02-17 22:03:21 -0700

    updated requirements and setting.py

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='your-current-secret-key-here')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-tp@t!u^h-95#!#!(t!-s5&u+70=isidk2!s6^3o9f552w3vk76')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
